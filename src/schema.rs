39ea2b4d501e0c39f90e9eeaa3bb88dea9f118a2
raspadrone <robertfinley@protonmail.com>
2025-06-16 01:40:21 +0200

    diesel orm setup

---
+++ src/schema.rs
@@ -0,0 +1,42 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    _sqlx_migrations (version) {
+        version -> Int8,
+        description -> Text,
+        installed_on -> Timestamptz,
+        success -> Bool,
+        checksum -> Bytea,
+        execution_time -> Int8,
+    }
+}
+
+diesel::table! {
+    passwords (id) {
+        id -> Uuid,
+        #[max_length = 255]
+        key -> Varchar,
+        value -> Text,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+        user_id -> Uuid,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        id -> Uuid,
+        #[max_length = 255]
+        username -> Varchar,
+        hashed_password -> Text,
+        created_at -> Timestamptz,
+    }
+}
+
+diesel::joinable!(passwords -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    _sqlx_migrations,
+    passwords,
+    users,
+);
